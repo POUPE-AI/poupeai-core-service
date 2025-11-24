cfe1a81cad5eebab38a6e0bb7194fd1ddebb47a3
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2025-11-23 22:51:18 -0300

    feat(app): configure application properties

--- poupeai-core-application/src/main/java/io/github/poupeai/core/application/Application.java
+++ poupeai-core-application/src/main/java/io/github/poupeai/core/application/Application.java
@@ -2,8 +2,12 @@ package io.github.poupeai.core.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "io.github.poupeai.core")
+@EntityScan(basePackages = "io.github.poupeai.core.persistence.entity")
+@EnableJpaRepositories(basePackages = "io.github.poupeai.core.persistence.repository")
 public class Application {
 
 	public static void main(String[] args) {
