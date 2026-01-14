571d5c3071c766c8e643650384612fb62b1e7b3a
Erisvaldo <erisvaldoapps@gmail.com>
2026-01-14 17:24:25 -0300

    feat(bankaccounts): add domain model

---
+++ poupeai-core-domain/src/main/java/io/github/poupeai/core/domain/model/BankAccount.java
@@ -0,0 +1,26 @@
+package io.github.poupeai.core.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankAccount {
+    private UUID id;
+    private UUID profileId;
+    private Long institutionId;
+    private String name;
+    private String description;
+    private BigDecimal initialBalance;
+    private Boolean isDefault;
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+}
