cc51569d6b2608b9b130f6666f8dd8c7f2c642d9
Erisvaldo <erisvaldoapps@gmail.com>
2026-02-02 15:48:07 -0300

    feat: add profile deactivation/reactivation and profile deletion

---
+++ src/main/java/io/github/poupeai/core/persistence/config/KeycloakConfig.java
@@ -0,0 +1,39 @@
+package io.github.poupeai.core.persistence.config;
+
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+
+    @Value("${keycloak.admin.server-url}")
+    private String serverUrl;
+
+    @Value("${keycloak.admin.realm}")
+    private String realm;
+
+    @Value("${keycloak.admin.client-id}")
+    private String clientId;
+
+    @Value("${keycloak.admin.username}")
+    private String username;
+
+    @Value("${keycloak.admin.password}")
+    private String password;
+
+    @Bean
+    public Keycloak keycloakAdminClient() {
+        return KeycloakBuilder.builder()
+                .serverUrl(serverUrl)
+                .realm("master")
+                .clientId(clientId)
+                .username(username)
+                .password(password)
+                .grantType(OAuth2Constants.PASSWORD)
+                .build();
+    }
+}
